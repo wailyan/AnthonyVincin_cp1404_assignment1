dfba56f272a1f40e7b48780c652173a0aa71bdbe
AnthonyV01 <anthony.vincin@hotmail.com>
2017-09-01 17:24:16 +1000

    Final commit, comments on functions
    This commit includes the comments on the functions as well as the
    information in the docstring.

--- a1-AnthonyV01/a1_test.py
+++ a1-AnthonyV01/a1_test.py
@@ -2,13 +2,37 @@
 ' assignment1.py ' by Anthony Vincin
 Date : 1/09/2017 
 GitHub URL: https://github.com/AnthonyV01/AnthonyVincin_cp1404_assignment1
+GitHub URL (private repo): https://github.com/CP1404-2017-2/a1-AnthonyV01
 
+Pseudocode for two functions:
+LOAD SONG:
+return_song_contents(read_file):
+    open file to read, this value is passed into the function
+    count is set to 0
+    song_info = list
+    for each line in file
+        count = count + 1
+        song_info = song_info + line
+    return song_info, count
+    
+COMPLETE A SONG:
+learn_song(song_info, count):
+    input = song to be learnt
+    if input not on list
+        invalid input
+    elif 
+        song has already been learnt
+    else
+        song is now learnt
 """
 song_file = "songs.csv"
 open_song_file = open(song_file, 'r')
 
 
 def list_song_contents(song_info):
+    """
+    Lists each song in addition to the status of the song (learnt or un-learnt)
+    """
     for i in range(0, len(song_info), 1):
         print("{:2} ".format(i), end="")
         if song_info[i][len(song_info[i])-1] == 'y':
@@ -19,6 +43,9 @@ def list_song_contents(song_info):
 
 
 def return_song_contents(read_file):
+    """
+    Takes the read file as a parameter and appends each of the songs to the list (song_info)
+    """
     song_counter = 0
     song_info = []
     for line in read_file:
@@ -29,15 +56,10 @@ def return_song_contents(read_file):
     return song_info, song_counter
 
 
-def return_song_name(song_info):
-    line_count = 0
-    for line in song_info:
-        line_count += 1
-        each_song_name = line.split(',')
-        print(each_song_name[0])
-
-
-def learn_song(song_info, song_counter):
+def learn_song(song_list, song_counter):
+    """
+    Changes the status of a song in the list (learnt or not), does error checking within the function
+    """
     valid_input = False
     number = 0
     while not valid_input:
@@ -52,14 +74,17 @@ def learn_song(song_info, song_counter):
         except ValueError:
             valid_input = False
             print("Invalid input; enter a valid number.")
-    if song_info[number][len(song_info[number]) - 1] == 'y':
-        song_info[number][len(song_info[number]) - 1] = 'n'
-        print(song_info[number][0], "learned.")
+    if song_list[number][len(song_list[number]) - 1] == 'y':
+        song_list[number][len(song_list[number]) - 1] = 'n'
+        print(song_list[number][0], "learned.")
     else:
         print("Song already learnt.")
 
 
 def add_song(song_info, song_counter):
+    """
+    Takes user input to append a song to the list, does error checking within the function
+    """
     new_song = []
     title_of_song = input("Title: ").strip()
     while len(title_of_song) < 1:
@@ -86,10 +111,12 @@ def add_song(song_info, song_counter):
 
 
 def main():
+    """
+    Takes user input to the menu, decides which path the user takes, opens and closes the files for reading/writing
+    """
     read_file = open(song_file, 'r')
     song_info, song_counter = return_song_contents(read_file)
     read_file.close()
-    # return_song_name(song_info) #
     print("Songs To Learn 1.0 - by Anthony Vincin")
     print("{} songs loaded".format(song_counter))
     menu = """
@@ -115,7 +142,7 @@ def main():
     print("Have a nice day :)")
     write_song_file = open(song_file, 'w')
     for line in song_info:
-        print("{},{},{},{}".format(line[0], line[1], line[2], line[3]),file=write_song_file)
+        print("{},{},{},{}".format(line[0], line[1], line[2], line[3]), file=write_song_file)
     open_song_file.close()
     read_file.close()
 if __name__ == '__main__':
